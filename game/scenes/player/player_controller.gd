6d0908f862b31740a3d2658a75b9699cf3619b9c
Pablo <pablomillancb@gmail.com>
2026-01-12 21:22:35 +0100

    add player scene

---
+++ game/scenes/player/player_controller.gd
@@ -0,0 +1,12 @@
+@icon("res://icons/16x16/button.png")
+extends Node
+class_name PlayerController
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
