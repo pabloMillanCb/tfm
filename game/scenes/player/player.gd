56c09fff710e543408f0ad61fd7a04639d5f94da
Pablo <pablomillancb@gmail.com>
2026-01-12 23:03:55 +0100

    add move state to player

--- game/scenes/player/player.gd
+++ game/scenes/player/player.gd
@@ -1,6 +1,17 @@
 extends CharacterBody2D
 class_name Player
+
+@export var debug_mode = false
+
+@export var walking_speed = 100
+@export var walking_acceleration = 400
+
+@onready var state_machine: StateMachine = (func get_state_machine() -> StateMachine:
+	return get_node("StateMachine")
+).call()
+
 func _process(delta: float) -> void:
 	%DebugStateName.visible = debug_mode
+
 func set_animation(animation_name: String):
 	$PlayerSprites.play(animation_name)
--- game/scenes/player/states/move.gd
+++ game/scenes/player/states/move.gd
@@ -1,11 +1,34 @@
 extends PlayerState
 
+var current_speed = 0.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _enter(_previous_state_path: String, _init_data := {}):
+	player.set_animation("move")
+	current_speed = player.velocity.x
+	player.get_node("DebugStateName").text = MOVE
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _update_physics(_delta):
+	
+	var direction = Input.get_axis("move_left", "move_right")
+	
+	if direction:
+		current_speed = move_toward(
+			current_speed, 
+			player.walking_speed * direction, 
+			_delta * player.walking_acceleration) 
+	else:
+		current_speed = move_toward(
+			current_speed, 
+			0.0, 
+			_delta * player.walking_acceleration) 
+		
+	player.velocity.x = current_speed
+	
+	player.move_and_slide()
+
+	if player.velocity.x == 0.0 and !direction:
+		finished.emit(IDLE)
+
+func _exit():
+	player.velocity.x = 0.0
