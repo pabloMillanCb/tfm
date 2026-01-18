f493fe741b105a4c5e9f6c2eef20db04debbd5d4
Pablo <pablomillancb@gmail.com>
2026-01-18 17:28:43 +0100

    change player speed

--- game/scenes/player/player.gd
+++ game/scenes/player/player.gd
@@ -4,7 +4,7 @@ class_name Player
 @export var debug_mode = false
 
 @export var walking_speed = 80
-@export var walking_acceleration = 400
+@export var walking_acceleration = 660
 @export var jump_force = -120
 
 @onready var state_machine: StateMachine = (func get_state_machine() -> StateMachine:
