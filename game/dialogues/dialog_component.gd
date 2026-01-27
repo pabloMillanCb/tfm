0d6a0be54d4bfc25e8f49531d0b4dcdde4d627ca
Pablo <pablomillancb@gmail.com>
2026-01-27 21:08:08 +0100

    add dialog component

---
+++ game/dialogues/dialog_component.gd
@@ -0,0 +1,8 @@
+extends Area2D
+class_name DialogComponent
+
+@export var dialog_script = preload("res://dialogues/test_dialog/test_dialogue.dialogue")
+@export var dialog_balloon = preload("res://dialogues/balloon.tscn")
+
+func _ready() -> void:
+	pass
