4699f0b3a5f6cbe00ed04bb249f18253330443b5
Pablo <pablomillancb@gmail.com>
2026-02-07 17:22:55 +0100

    update save file playtime from Clock

--- game/autoload/data_manager.gd
+++ game/autoload/data_manager.gd
@@ -13,20 +13,21 @@ func _ready() -> void:
 func save_game_in_room(slot: int) -> bool:
 	var room_id = MetSys.get_current_room_id()
 	var room_path = ResourceUID.path_to_uid(room_id)
-	print(room_path)
+
 	var new_save = current_save
 	new_save.last_room = room_path
+	new_save.play_time += Clock.current_time_count
 	var success = save_game_data(slot, new_save)
 	
 	if success:
 		current_save = new_save
+		GameEvent._on_game_saved.emit()
 		return true
 	else:
 		return false
 
 
 func save_game_data(slot: int, save_data: SaveData) -> bool:
-	print("game saved")
 	if slot >= 0 and slot <= 3:
 		var save = ResourceSaver.save(save_data, _get_save_path(slot))
 		return save == OK
