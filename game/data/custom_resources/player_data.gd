bfd2409b26b3ccfa5bbe8c10984bfe52b8181130
Pablo <pablomillancb@gmail.com>
2026-02-09 17:11:02 +0100

    add player data default values

--- game/data/custom_resources/player_data.gd
+++ game/data/custom_resources/player_data.gd
@@ -1,13 +1,13 @@
 extends Resource
 class_name PlayerData
 
-@export var max_health: int
+@export var max_health: int = 2
 
-@export var has_sword_update: bool
-@export var has_break_update: bool
-@export var has_beam_update: bool
-@export var has_pogo_update: bool
-@export var has_teleport_update: bool
+@export var has_sword_update: bool = false
+@export var has_break_update: bool = false
+@export var has_beam_update: bool = false
+@export var has_pogo_update: bool = false
+@export var has_teleport_update: bool = false
 
 func _init(p_sword_update = false, p_break_update = false, 
 			p_beam_update = false, p_pogo_update = false,
@@ -17,6 +17,6 @@ func _init(p_sword_update = false, p_break_update = false,
 	
 	has_sword_update = p_sword_update
 	has_break_update = p_break_update
-	has_break_update = p_break_update
+	has_beam_update = p_beam_update
 	has_pogo_update = p_pogo_update
 	has_teleport_update = p_teleport_update
