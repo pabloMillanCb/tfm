2c587014d0c719269b2f9f724af2580574b4a169
Pablo <pablomillancb@gmail.com>
2026-02-17 17:50:13 +0100

    sync settings values with real values

--- game/autoload/screen_environment.gd
+++ game/autoload/screen_environment.gd
@@ -6,6 +6,14 @@ func set_brightnes(value: float):
 		environment.adjustment_brightness = value
 
 
+func get_brightness() -> float:
+	return environment.adjustment_brightness
+
+
 func set_contrast(value: float):
 	if value >= 0.75 and value <= 1.25:
 		environment.adjustment_contrast = value
+
+
+func get_contrast() -> float:
+	return environment.adjustment_contrast
--- game/scenes/menu/settings/settings_menu.gd
+++ game/scenes/menu/settings/settings_menu.gd
@@ -3,11 +3,26 @@ class_name SettingsMenu
 
 signal exited
 
+var master_bus_index = AudioServer.get_bus_index("Master")
+var music_bus_index = AudioServer.get_bus_index("Music")
+var sfx_bus_index = AudioServer.get_bus_index("SFX")
+
 func _ready() -> void:
+	# Languages
 	$CenterContainer/VBoxContainer/Back.grab_focus()
 	for lang in LocalizationManager.Language.values():
 		%LanguageSelection.add_item(LocalizationManager.language_name.get(lang), lang) 
 	%LanguageSelection.select(LocalizationManager.get_current_language_index())
+	
+	# Screen
+	%FullscreenCheckbox.button_pressed = DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN
+	%BrightnessSlider.value = ScreenEnvironment.get_brightness()
+	%ContrastSlider.value = ScreenEnvironment.get_contrast()
+	
+	# Music
+	%MasterVolSlider.value = AudioServer.get_bus_volume_linear(master_bus_index)
+	%MusicVolSlider.value = AudioServer.get_bus_volume_linear(music_bus_index)
+	%SfxVolSlider.value = AudioServer.get_bus_volume_linear(sfx_bus_index)
 
 
 func _on_back_pressed() -> void:
@@ -26,17 +41,14 @@ func _on_fullscreen_checkbox_toggled(toggled_on: bool) -> void:
 
 
 func _on_master_vol_slider_value_changed(value: float) -> void:
-	var master_bus_index = AudioServer.get_bus_index("Master")
 	AudioServer.set_bus_volume_db(master_bus_index, linear_to_db(value))
 
 
 func _on_music_vol_slider_value_changed(value: float) -> void:
-	var music_bus_index = AudioServer.get_bus_index("Music")
 	AudioServer.set_bus_volume_db(music_bus_index, linear_to_db(value))
 
 
 func _on_sfx_vol_slider_value_changed(value: float) -> void:
-	var sfx_bus_index = AudioServer.get_bus_index("SFX")
 	AudioServer.set_bus_volume_db(sfx_bus_index, linear_to_db(value))
 
 
