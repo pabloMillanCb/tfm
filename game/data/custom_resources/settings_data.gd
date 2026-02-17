d2d7d570c78aa6a23ecfcc13d274fa848f64e6e2
Pablo <pablomillancb@gmail.com>
2026-02-17 18:39:44 +0100

    add game settings resource

---
+++ game/data/custom_resources/settings_data.gd
@@ -0,0 +1,20 @@
+extends Resource
+class_name SettingsData
+
+@export var language: String
+@export var full_screen_enable: bool
+@export var master_volume: float
+@export var music_volume: float
+@export var sfx_volume: float
+@export var screen_brightness: float
+@export var screen_contrast: float
+
+#Controls is managed in input_mapper.gd
+func _init() -> void:
+	language = ""
+	full_screen_enable = false
+	master_volume = 0.8
+	music_volume = 0.8
+	sfx_volume = 0.8
+	screen_brightness = 1.0
+	screen_contrast = 1.0
