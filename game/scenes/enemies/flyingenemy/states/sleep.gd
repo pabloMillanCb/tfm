c048af88bec03f2c4135cc4c05ce3ad4b2434490
Pablo <pablomillancb@gmail.com>
2026-02-18 19:53:23 +0100

    add sleep state

--- game/scenes/enemies/flyingenemy/states/sleep.gd
+++ game/scenes/enemies/flyingenemy/states/sleep.gd
@@ -1,1 +1,10 @@
 extends TriangleFishState
+
+
+func _enter(_previous_state_path: String, _init_data := {}):
+	fish.set_animation("sleep")
+
+
+func _update(_delta):
+	if fish.player != null:
+		finished.emit(WATCH)
--- game/scenes/enemies/flyingenemy/triangle_fish.gd
+++ game/scenes/enemies/flyingenemy/triangle_fish.gd
@@ -1,2 +1,14 @@
 extends EnemyCharacter
 class_name TriangleFish
+
+
+@export var speed = 100.0
+var player: Player
+
+
+func set_animation(animation: String):
+	$AnimatedSprite2D.play(animation)
+
+
+func _on_player_detected(body: Player) -> void:
+	player = body
