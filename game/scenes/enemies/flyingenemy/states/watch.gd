b22215a42eb9d211b7ff9671e6003cb7ecd5d961
Pablo <pablomillancb@gmail.com>
2026-02-18 20:02:49 +0100

    add watch state

--- game/scenes/enemies/flyingenemy/states/watch.gd
+++ game/scenes/enemies/flyingenemy/states/watch.gd
@@ -1,1 +1,15 @@
 extends TriangleFishState
+
+var ray_cast: RayCast2D
+
+func _enter(_previous_state_path: String, _init_data := {}):
+	fish.set_animation("watch")
+	ray_cast = fish.ray_cast
+
+func _update_physics(_delta):
+	ray_cast.target_position = fish.player.global_position
+	
+	fish.update_direction()
+	
+	if ray_cast.get_collider() is Player:
+		finished.emit(CHASE)
--- game/scenes/enemies/flyingenemy/triangle_fish.gd
+++ game/scenes/enemies/flyingenemy/triangle_fish.gd
@@ -3,7 +3,14 @@ class_name TriangleFish
 
 
 @export var speed = 100.0
+
+@onready var ray_cast: RayCast2D = $RayCast2D
+@onready var state_machine: StateMachine = (func get_state_machine() -> StateMachine:
+	return get_node("StateMachine")
+).call()
+
 var player: Player
+var can_take_hits = true
 
 
 func set_animation(animation: String):
@@ -12,3 +19,17 @@ func set_animation(animation: String):
 
 func _on_player_detected(body: Player) -> void:
 	player = body
+
+
+func update_direction():
+	var direction = global_position.direction_to(player.global_position)
+	if direction.x > 0:
+		$AnimatedSprite2D.flip_h = false
+	else:
+		$AnimatedSprite2D.flip_h = true
+
+
+func take_damage():
+	print("ouch")
+	if can_take_hits:
+		state_machine._transition_to_next_state(TriangleFishState.HIT)
