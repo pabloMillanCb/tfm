5c71a22d527c3707c4cb579cf4dec2b20fd23266
Pablo <pablomillancb@gmail.com>
2026-02-21 20:34:16 +0100

    add arroy indicator to dialog component

--- game/dialogues/dialog_component.gd
+++ game/dialogues/dialog_component.gd
@@ -5,14 +5,21 @@ class_name DialogueComponent
 @export var dialog_balloon = preload("res://dialogues/balloon.tscn")
 
 var player: Player
+var talking = false
 
 func _process(delta: float) -> void:
+	$Arrow.visible = player != null and !talking
+	
 	if Input.is_action_just_pressed("aim_up") and player != null:
 		player.dialog_start_request.emit(self)
 
 
 func show_dialog():
 	DialogueManager.show_dialogue_balloon_scene(dialog_balloon, dialog_script)
+	talking = true
+	DialogueManager.dialogue_ended.connect(func(_resource): 
+		talking = false
+	)
 
 
 func _on_body_entered(body: Node2D) -> void:
