47d342f2019b1adfae1b430dc6cf10ba5fc98b67
Pablo <pablomillancb@gmail.com>
2026-02-22 19:13:11 +0100

    apply theme in all menus

--- game/scenes/menu/select_file/SaveFileCard/save_file_card.gd
+++ game/scenes/menu/select_file/SaveFileCard/save_file_card.gd
@@ -3,8 +3,8 @@ class_name SaveFileCard
 func set_data(data: SaveData, slot: int):
 	
 	if data != null:
-		$FileName.text = "Saved data " + str(slot+1)
-		$PlaytimeValue.text = str(data.play_time)
+		%FileName.text = "Saved data " + str(slot+1)
+		%PlaytimeValue.text = str(data.play_time)
 	else:
-		$FileName.text = "no data"
-		$PlaytimeValue.text = "-"
+		%FileName.text = "no data"
+		%PlaytimeValue.text = "-"
