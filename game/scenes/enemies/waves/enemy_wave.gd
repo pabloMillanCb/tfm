49c7686ccc87fa4fc5b69cbcd67b421a5ffd4548
Pablo <pablomillancb@gmail.com>
2026-02-24 18:33:41 +0100

    add enemy waves

---
+++ game/scenes/enemies/waves/enemy_wave.gd
@@ -0,0 +1,41 @@
+@tool
+extends Node2D
+class_name EnemyWave
+
+@export var enemies: Resource
+@export var appear_particle: Resource
+var wave: Node2D
+
+signal start_wave
+signal on_wave_end
+
+func _ready() -> void:
+	MetSys.register_storable_object(self)
+	start_wave.connect(func():
+		wave = enemies.instantiate()
+		for enemy in wave.get_children():
+			print(enemy.position)
+			var smoke: CPUParticles2D = appear_particle.instantiate()
+			add_child(smoke)
+			smoke.position = enemy.position
+			smoke.restart()
+		add_child(wave)
+	)
+
+func _process(delta: float) -> void:
+	if wave != null and wave.get_children().size() == 0:
+		on_wave_end.emit()
+		MetSys.store_object(self)
+		queue_free()
+
+
+func _get_configuration_warnings():
+	var warnings = []
+
+	if get_node("Enemies") == null and enemies == null:
+		warnings.append("Add a Node2D called Enemies with all the enemies as their child")
+	elif enemies == null:
+		warnings.append("Assign a Node2D to the enemies parameter")
+
+	# Returning an empty array means "no warning".
+	return warnings
