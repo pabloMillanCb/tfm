16b3c2da48e3050852701649ccf871d2f0de6cb2
Pablo <pablomillancb@gmail.com>
2026-02-28 21:07:32 +0100

    adjust triangle fish chase

--- game/scenes/enemies/flyingenemy/states/chase.gd
+++ game/scenes/enemies/flyingenemy/states/chase.gd
@@ -18,36 +18,44 @@ func _enter(_previous_state_path: String, _init_data := {}):
 func _update_physics(_delta):
 	
 	ray_cast.target_position = vector_to_player()
-	
 	agent.target_position = player.global_position
 	
 	if agent.distance_to_target() > 0:
-		#direction.x =  move_toward(direction.x, get_direction_to_chase().x, _delta * 2)
-		#direction.y = move_toward(direction.y, get_direction_to_chase().y, _delta * 1.5)
 		direction = direction.move_toward(get_direction_to_chase(), _delta * 1.5)
-		#direction.y = get_direction_to_chase().y
 
-	#fish.velocity.x = direction.x * fish.speed
-	#fish.velocity.y = direction.y * fish.speed * 1.5
 	fish.velocity = direction * fish.speed
 	fish.update_direction(fish.velocity)
 	
 	fish.move_and_slide()
+	#var collision = fish.move_and_collide(fish.velocity.normalized())
+	#if collision:
+	#	var collider = collision.get_collider_velocity()
+	#	fish.velocity = -10*collider
+		#fish.velocity = -fish.velocity.normalized() * 300
+	#	direction = fish.velocity.normalized()
 	
 
 func get_direction_to_chase() -> Vector2:
 	if abs(fish.global_position.y - player.global_position.y) > 10 and ray_cast.get_collider() is Player:
-		return Vector2(sign(player.global_position.x - fish.global_position.x)*0.5,
-			sign(player.global_position.y - fish.global_position.y)).normalized()
-	elif fish.global_position.y == player.global_position.y and ray_cast.get_collider() is Player:
+		$"../../Label".text = "1"
+		return fish.global_position.direction_to(get_ground_quarter_point()).normalized()
+	elif abs(fish.global_position.y - player.global_position.y) <= 10 and ray_cast.get_collider() is Player:
+		$"../../Label".text = "2"
 		return fish.global_position.direction_to(player.global_position).normalized()
 	else:
+		$"../../Label".text = "3"
 		return (agent.get_next_path_position() - fish.global_position).normalized()
 
 
 func get_ground_middle_point():
-	return Vector2(player.global_position.x - fish.global_position.x, player.global_position.y)
+	var x_middle_point = (player.global_position.x + fish.global_position.x)/2
+	return Vector2(x_middle_point, player.global_position.y)
+
+
+func get_ground_quarter_point():
+	var x_quarter_point = (get_ground_middle_point().x + fish.global_position.x)/2
+	return Vector2(x_quarter_point, player.global_position.y)
 
 
 func vector_to_player() -> Vector2:
-	return fish.player.global_position - fish.global_position
+	return (fish.player.global_position - fish.global_position) * 2
