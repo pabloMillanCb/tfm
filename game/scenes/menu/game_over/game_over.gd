336c57b09bf69a10806cfc8d3941894a62296939
Pablo <pablomillancb@gmail.com>
2026-03-20 16:49:58 +0100

    playtest hotfixes

--- game/scenes/menu/game_over/game_over.gd
+++ game/scenes/menu/game_over/game_over.gd
@@ -10,6 +10,7 @@ func _on_main_menu_pressed() -> void:
 
 func _on_retry_pressed() -> void:
 	$AnimatedSprite2D.play("open")
+	$AudioStreamPlayer.play()
 	await get_tree().create_timer(1.5).timeout
 	DataManager.reload_current_save()
 	GameEvent._on_player_respawn.emit()
