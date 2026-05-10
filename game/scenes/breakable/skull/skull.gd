5555c12d618c78f3a2b1d2f16c7bf82d5c73c9c6
Pablo <pablomillancb@gmail.com>
2026-05-10 16:52:49 +0200

    add particles to breakables

--- game/scenes/breakable/skull/skull.gd
+++ game/scenes/breakable/skull/skull.gd
@@ -6,8 +6,15 @@ func _ready() -> void:
 	if !permanent:
 		MetSys.register_storable_object(self)
 
+func place_particles():
+	var particles = preload("res://scenes/breakable/breakable_particles.tscn").instantiate()
+	particles.is_wall = false
+	particles.global_position = global_position
+	get_parent().add_child(particles)
+
 func take_damage(direction: Vector2 = Vector2.ZERO):
 	print("take_damage")
+	place_particles()
 	Input.start_joy_vibration(0,0.4,0.4,0.1)
 	if !permanent:
 		MetSys.store_object(self)
--- game/scenes/breakable/wall/wall_breakable.gd
+++ game/scenes/breakable/wall/wall_breakable.gd
@@ -13,6 +13,13 @@ func _ready() -> void:
 	MetSys.register_storable_object(self)
 
 func take_damage(_vector: Vector2):
+	place_particles()
 	Input.start_joy_vibration(0,0.8,0.8,0.4)
 	MetSys.store_object(self)
 	queue_free()
+
+func place_particles():
+	var particles = preload("res://scenes/breakable/breakable_particles.tscn").instantiate()
+	particles.is_wall = false
+	particles.global_position = global_position
+	get_parent().add_child(particles)
