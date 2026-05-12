f657c31c1e71a3cb320b7348424dba1e13c94c46
Pablo <pablomillancb@gmail.com>
2026-05-12 21:54:02 +0200

    add f

--- game/scenes/world/rooms/starting_area/h.gd
+++ game/scenes/world/rooms/starting_area/h.gd
@@ -3,14 +3,14 @@ extends Node2D
 
 func _ready() -> void:
 	if DataManager.is_event_registered("h"):
-		$Sprite2D.position.y = -119.22
+		$Sprite2D.queue_free()
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	$AnimationPlayer.play("up")
-	#TODO: sonidorision
+	DataManager.register_event("h", true)
+
 
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	DataManager.register_event("h", true)
 	$Sprite2D.queue_free()
